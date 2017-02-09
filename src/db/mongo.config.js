c5697cf8d60126cd7130d4f772ad3794124c2ff2
Nhan Dang <akitoeki@gmail.com>
2017-02-09 13:55:25 +0700

    sample full mongo config

--- src/db/mongo.config.js
+++ src/db/mongo.config.js
@@ -3,4 +3,30 @@ module.exports = {
     uri: 'localhost:27017',
     database: 'hapi-mongose-test',
   },
+  production: {
+    uri: 'localhost:27017,localhost:27018,localhost:27019',
+    database: 'hapi-mongoose-test',
+    isReplSet: true,
+    replSetName: 'MyRep',
+    auth: {
+      user: 'test',
+      password: 'test',
+    },
+    options: {
+      replset: {
+        strategy: 'ping',
+        readSecondary: true,
+        socketOptions: {
+          keepAlive: 1,
+        },
+      },
+      server: {
+        readPreference: 'secondary',
+        auto_reconnect: true,
+        socketOptions: {
+          keepAlive: 1,
+        },
+      },
+    },
+  },
 }
